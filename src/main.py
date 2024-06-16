3fd71a9fb38eb0458bb7000680adac257abfee00
Simtimus <simtimusbos@gmail.com>
2024-06-16 13:45:45 +0300

    Updated callbacks logic to slots and signals.

--- src/main.py
+++ src/main.py
@@ -10,9 +10,11 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 
+from calculations.operation import OperationManager
 from config import load_config, setup_logging
 from gui.windows.MainWindow import MainWindow
 from calculations.vicon_nexus import ViconNexusAPI, Marker
+
 
 def main():
     setup_logging()
@@ -21,13 +23,11 @@ def main():
     logger = logging.getLogger('my_app')
     logger.info("Application started")
 
-    # nexus_api = ViconNexusAPI()
-    # subject_names: list[str] = nexus_api.GetSubjectNames()
-    # markers: dict[str, Marker] = nexus_api.GetMarkers(subject_names[0])
+    nexus_api = ViconNexusAPI()
 
     app = QApplication(sys.argv)
 
-    main_window = MainWindow(logger, config)
+    main_window = MainWindow(logger, config, nexus_api)
     main_window.show()
 
     code: int = app.exec()
