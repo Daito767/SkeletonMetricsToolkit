6a8123f265a71a101b4eed54645dae79a56259a6
Simtimus <simtimusbos@gmail.com>
2024-06-16 23:23:53 +0300

    Added selectable variables base on selected operation

--- src/gui/windows/CreateOperationDialog.py
+++ src/gui/windows/CreateOperationDialog.py
@@ -6,12 +6,14 @@ Created on June 2024
 """
 from logging import Logger
 
-from PySide6.QtCore import Qt, QSize
+from PySide6.QtCore import Qt, Signal, Slot
 from PySide6.QtGui import QFont
 from PySide6.QtWidgets import QMainWindow, QWidget, QDialog, QVBoxLayout, QLabel, QListWidget, QPushButton, QComboBox, \
     QTextEdit, QLineEdit, QHBoxLayout, QSpacerItem, QSizePolicy
 from calculations.operation import OperationManager
+from calculations.vicon_nexus import ViconNexusAPI, Marker
 import logging
+import inspect
 
 
 class CreateOperationDialog(QDialog):
@@ -19,6 +21,7 @@ class CreateOperationDialog(QDialog):
         self.logger: logging.Logger = logger
         self.logger.info("Initializing CreateOperation Dialog")
         self.operation_manager: OperationManager = operation_manager
+        self.markers: dict[str, Marker] = {}
 
         super().__init__(parent)  # Initialize QDialog
 
@@ -34,8 +37,13 @@ class CreateOperationDialog(QDialog):
 
         self.operation_description_label: QLabel = QLabel("Description:")
 
-        self.line_edit_label: QLabel = QLabel("Result Name:")
+        self.dynamic_layout: QVBoxLayout = QVBoxLayout()
+        self.parameter_dropdowns: list[QComboBox] = []
+
+        self.result_name_label: QLabel = QLabel("Result Name:")
         self.line_edit: QLineEdit = QLineEdit(self)
+
+        self.add_operation_button: QPushButton = QPushButton("Add")
 
         self.build()
         self.setup_ui()
@@ -43,6 +51,7 @@ class CreateOperationDialog(QDialog):
     def build(self):
         self.dropdown_menu.currentIndexChanged.connect(self.update_interface)
         self.dropdown_menu.addItems(self.operation_manager.available_functions.keys())
+        self.add_operation_button.clicked.connect(self.add_operation)
 
     def setup_ui(self):
         self.setWindowTitle("Create Operation")
@@ -60,22 +69,51 @@ class CreateOperationDialog(QDialog):
         self.row_dropdown.addWidget(self.dropdown_label)
         self.row_dropdown.addWidget(self.dropdown_menu)
 
-        self.line_edit_label.setFont(QFont('Arial', 11))
+        self.layout.addLayout(self.dynamic_layout)
+
+        self.result_name_label.setFont(QFont('Arial', 11))
         self.line_edit.setFont(QFont('Arial', 11))
-        self.row_line_edit.addWidget(self.line_edit_label)
+        self.row_line_edit.addWidget(self.result_name_label)
         self.row_line_edit.addWidget(self.line_edit)
+
+        self.layout.addWidget(self.add_operation_button)
 
         self.setLayout(self.layout)
 
-    def update_interface(self, index):
+    def update_interface(self):
         operation_name = self.dropdown_menu.currentText()
         if operation_name in self.operation_manager.available_functions:
             operation_description = self.operation_manager.available_functions[operation_name]
             self.operation_description_label.setText(operation_description.__doc__)
+            self.update_dynamic_layout()
+
+    def update_dynamic_layout(self):
+        self.parameter_dropdowns.clear()
+        for i in reversed(range(self.dynamic_layout.count())):
+            widget_to_remove = self.dynamic_layout.itemAt(i).widget()
+            self.dynamic_layout.removeWidget(widget_to_remove)
+            widget_to_remove.setParent(None)
+
+        func: FunctionType = self.operation_manager.available_functions[self.dropdown_menu.currentText()]
+        params = inspect.getfullargspec(func).args
+
+        for param in params:
+            dropdown: QComboBox = QComboBox()
+            dropdown.addItems(self.markers)
+            self.parameter_dropdowns.append(dropdown)
+            self.dynamic_layout.addWidget(dropdown)
+
+    def add_operation(self):
+        pass
 
     def showEvent(self, event):
         super().showEvent(event)
         self.run_on_show()
 
     def run_on_show(self):
+        self.update_interface()
         self.line_edit.setText("")
+
+    @Slot('QVariant')
+    def update_markers(self, markers: dict[str, Marker]):
+        self.markers = markers
